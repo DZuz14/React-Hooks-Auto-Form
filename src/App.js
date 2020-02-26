ad5da932e9e5ba1d20ef40d094103c4232457366
Dan <danielzuzevich@gmail.com>
2020-02-26 12:30:47 -0500

    yolo

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
 /** @jsx jsx */
 import React, { useState } from 'react'
 import { css, jsx } from '@emotion/core'
-import AutoForm from './components/AutoForm'
-import form from './forms'
 
-const App = () => {
-  const [status, setStatus] = useState(null)
-
-  const handleSubmit = form => {
-    console.log(form)
-    setStatus('loading')
-
-    setTimeout(() => {
-      setStatus('success')
-      // setStatus('error')
-    }, 3000)
-  }
-
-  return (
-    <div
-      style={{
-        padding: '140px 0 0 80px'
-      }}
-    >
-      <h1
-        css={css`
-          color: #fff;
-          font-size: 56px;
-          margin: 0 0 15px;
-        `}
-      >
-        Get VIP Access
-      </h1>
-
-      <AutoForm form={form} onSubmit={handleSubmit} status={status} />
-    </div>
-  )
-}
+const App = () => <div>{/*  */}</div>
 
 export default App
