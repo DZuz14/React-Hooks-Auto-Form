51fd1b8b4ff3752f1fd8a8d2811ba191e30c1f3b
Dan <danielzuzevich@gmail.com>
2020-02-26 10:34:31 -0500

    build folder

---
+++ build/precache-manifest.3a783db52ffd20397c70b0f364129b32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11265d32dbbefee3e2c173f4c10c960a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58091fbcda88eb898b85",
+    "url": "/static/js/2.b9029d2c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b9029d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e6fa7585169659458c",
+    "url": "/static/js/main.47fca564.chunk.js"
+  },
+  {
+    "revision": "18fb8bea87325ec82a92",
+    "url": "/static/js/runtime-main.19d75b1f.js"
+  }
+]);
